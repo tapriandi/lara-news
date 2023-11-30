537feee290990fdb54c805b0ca26626b8eca292b
tapriandi <tapriandi@gmail.com>
2023-11-30 09:01:18 +0700

    #fixing route

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,4 +1,27 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav class="flex items-center justify-between py-3 px-6 border-b border-gray-100">
+    <div id="nav-left" class="flex items-center">
+        <a href="{{ route('home') }}">
+            <x-application-logo />
+        </a>
+        <div class="top-menu ml-10">
+            <div class="flex space-x-4">
+                <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
+                    {{ __('Home') }}
+                </x-nav-link>
+            </div>
+        </div>
+    </div>
+    <div id="nav-right" class="flex items-center md:space-x-6">
+        @auth
+            @include('layouts.partials.header-right-auth')
+        @else
+            @include('layouts.partials.header-right-guest')
+        @endauth
+
+    </div>
+</nav>
+
+{{-- <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -216,4 +239,4 @@
             </div>
         </div>
     </div>
-</nav>
+</nav> --}}
