ae800674fbdaaee421f0ca192f94ec3f06133a84
tapriandi <tapriandi@gmail.com>
2023-11-30 17:51:39 +0700

    #6 Blog page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,12 +3,22 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'published_at' => 'datetime'
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
     public function scopePublished($query)
     {
@@ -17,5 +27,16 @@ class Post extends Model
     public function scopeFeatured($query)
     {
         $query->where('featured', true);
+    }
+
+    public function getExcerpt()
+    {
+        return Str::limit(strip_tags($this->body), 150);
+    }
+
+    public function getReadingTime()
+    {
+        $mins = round(str_word_count($this->body) / 250);
+        return ($mins < 1) ? 1 : $mins;
     }
 }
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -4,6 +4,6 @@
     $classes = $active ?? false ? 'lex space-x-2 items-center hover:text-blue-900 text-sm text-blue-500' : 'lex space-x-2 items-center hover:text-blue-900 text-sm text-gray-500';
 @endphp
 
-<a {{ $attributes->merge(['class' => $classes]) }}>
+<a wire:navigate {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
 </a>
