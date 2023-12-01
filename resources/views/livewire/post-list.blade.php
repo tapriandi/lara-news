bc1f616d1b48235ef47b9496d9edb7212afd3bbc
tapriandi <tapriandi@gmail.com>
2023-12-01 16:41:58 +0700

    #6 liveware component - sorting & serching

---
+++ resources/views/livewire/post-list.blade.php
@@ -0,0 +1,24 @@
+<div class=" px-3 lg:px-7 py-6">
+    <div class="flex justify-between items-center border-b border-gray-100">
+        <div class="text-gray-500">
+            @if ($search)
+                Searching {{ $search }}
+            @endif
+        </div>
+        <div id="filter-selector" class="flex items-center space-x-4 font-light ">
+            <button wire:click="setSort('desc')"
+                class="{{ $sort === 'desc' ? 'text-blue-500 border-b border-gray-700' : 'text-gray-500' }}  py-4">Latest</button>
+            <button wire:click="setSort('asc')"
+                class="{{ $sort === 'asc' ? 'text-blue-500 border-b border-gray-700' : 'text-gray-500' }}  py-4">Oldest</button>
+        </div>
+    </div>
+    <div class="py-4">
+        @foreach ($this->posts as $post)
+            <x-posts.post-item :post="$post" />
+        @endforeach
+    </div>
+
+    <div class="my-3">
+        {{ $this->posts->onEachSide(1)->links() }}
+    </div>
+</div>
